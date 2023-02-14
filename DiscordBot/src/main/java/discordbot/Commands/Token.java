a248d540ad9f0a7e86bf8d8de25000e8cc3fe418
Elisey <117991367+KES122@users.noreply.github.com>
2023-02-14 21:48:55 +0300

    Delete DiscordBot directory

--- DiscordBot/src/main/java/discordbot/Commands/Token.java
+++
@@ -1,9 +0,0 @@
-package discordbot.Commands;
-
-public class Token {
-    public static String token = "MTA3MjM5NTExMjI0NTY0NTM2Mg.G2T02W.ckn5rMJgHkLeZ81knEVTG7nJ4qSrQ3TD2XAp2E";
-
-    public Token() {
-    }
-
-}
